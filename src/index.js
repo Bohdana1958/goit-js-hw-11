addf6b88ce117d47fcfcfff9fedaabe8e95d5013
Bohdana1958 <139041084+Bohdana1958@users.noreply.github.com>
2023-12-10 18:52:47 +0100

    update api

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { getPhoto } from "./js/api";
+
+const form = document.querySelector('#search-form');
+const btn = document.querySelector('.button');
+const input = document.querySelector('#input_name');
+
+
+
